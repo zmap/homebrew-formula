9af30ce5ce851f9ad0c2395152e4d6afb751a011
David Adrian <davidcadrian@gmail.com>
2017-05-11 15:09:07 -0400

    rocksdb 3.10.2

---
+++ Formula/rocksdb@3.10.rb
@@ -0,0 +1,41 @@
+class RocksdbAT310 < Formula
+  homepage "http://rocksdb.org"
+  url "https://github.com/facebook/rocksdb/archive/rocksdb-3.10.2.tar.gz"
+  sha256 "5ace408b12e5e5c836c9ba0b1bd57662784d15820cd02b203459b3ac2e01fde7"
+  revision 1
+
+  needs :cxx11
+  depends_on "snappy"
+  depends_on "lz4"
+
+  def install
+    ENV.cxx11
+    system "make", "clean"
+    system "make", "static_lib"
+    system "make", "shared_lib"
+    system "make", "install", "INSTALL_PATH=#{prefix}"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <assert.h>
+      #include <rocksdb/options.h>
+      #include <rocksdb/memtablerep.h>
+      using namespace rocksdb;
+      int main() {
+        Options options;
+        options.memtable_factory.reset(
+                    NewHashSkipListRepFactory(16));
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "test.cpp", "-o", "db_test", "-v", "-std=c++11",
+                                "-stdlib=libc++",
+                                "-lstdc++",
+                                "-lrocksdb",
+                                "-lz", "-lbz2",
+                                "-lsnappy", "-llz4"
+    system "./db_test"
+  end
+end
